e46981d769ecbc639edac2e423c8d20ffe833d50
cwest9400 <chrstphrwest@gmail.com>
2023-04-29 19:16:21 -0400

    navbar set up

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px 24px 0px 24px;
+  
 }
 </style>
--- src/components/navbar/NavBarContent.vue
+++ src/components/navbar/NavBarContent.vue
@@ -1,10 +1,15 @@
 <template>
 <nav class="navbar">
-    <div class="navbar-content">
+    <div class="navbar-firstrow-content">
         <h1 class="title">
             six6wurm
         </h1>
 
+    </div>
+    <div class="navbar-secondrow-content">
+        <h4 class="tagline">
+            A blog about magic deck building
+        </h4>
     </div>
 </nav>
 </template>
@@ -16,11 +21,28 @@
 .navbar {
   position: fixed;
   top: 0;
-  left: 0;
-  right: 0;
-  background-color: #fff;
-  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.1);
+  right:0;
+  left:0;
+  background-color: #ffffff;
+  box-shadow: 0 2px 0px rgba(0, 0, 0, 0.1);
   z-index: 999;
+  padding: 16px;
+}
+.navbar-firstrow-content {
+    display: flex;
+    width: 100%;
+}
+.navbar-secondrow-content {
+    display: flex;
+    width: 100%;
+    font-weight: 300 ;
+}
+
+.title {
+    margin: 0;
+}
+.tagline {
+    margin: 0;
 }
 
 </style>
