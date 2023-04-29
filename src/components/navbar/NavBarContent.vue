27d148a635342e8a35b62069281065a04da52f4b
cwest9400 <chrstphrwest@gmail.com>
2023-04-29 18:53:17 -0400

    navbar set up

--- src/components/navbar/NavBarContent.vue
+++ src/components/navbar/NavBarContent.vue
@@ -1,7 +1,7 @@
 <template>
-<nav>
-    <div class="title">
-        <h1>
+<nav class="navbar">
+    <div class="navbar-content">
+        <h1 class="title">
             six6wurm
         </h1>
 
@@ -13,5 +13,14 @@
 </script>
 
 <style>
+.navbar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  background-color: #fff;
+  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.1);
+  z-index: 999;
+}
 
 </style>
