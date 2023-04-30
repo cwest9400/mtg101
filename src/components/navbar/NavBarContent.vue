4e8a263208cbd2272fcb3fb515fd91aba4f23ebd
cwest9400 <chrstphrwest@gmail.com>
2023-04-29 20:43:04 -0400

    hamburger menu

--- src/components/navbar/NavBarContent.vue
+++ src/components/navbar/NavBarContent.vue
@@ -4,12 +4,9 @@
         <h1 class="title">
             six6wurm
         </h1>
-
-    </div>
-    <div class="navbar-secondrow-content">
-        <h4 class="tagline">
-            A blog about magic deck building
-        </h4>
+        <div class="navbar-hamburger" @click="showMenu = !showMenu">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="currentColor" d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/></svg>
+        </div>
     </div>
 </nav>
 </template>
@@ -30,12 +27,12 @@
 }
 .navbar-firstrow-content {
     display: flex;
+    justify-content: space-between;
     width: 100%;
 }
-.navbar-secondrow-content {
+.navbar-hamburger {
     display: flex;
-    width: 100%;
-    font-weight: 300 ;
+    align-items: center;
 }
 
 .title {
