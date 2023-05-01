8981ab25462d161bdc32c57a6fe39f35f4aef48d
cwest9400 <chrstphrwest@gmail.com>
2023-04-30 23:45:46 -0400

    mock up - latest post card

--- src/components/MostRecentPost.vue
+++ src/components/MostRecentPost.vue
@@ -1,21 +1,44 @@
 <template>
+  <div class="hero-container">
+
     <div class="most-recent-post">
-<h1>Title of this really cool new blog post</h1>
-<p>by Magic Wizard</p>
-<button>
-    Read Now
-</button>
-</div>
+      <h1>Title of this really cool new blog post</h1>
+      <p>by Magic Wizard</p>
+      <button>Read Now</button>
+    </div>
+  </div>
 </template>
 <script>
-
 </script>
 
-<style>
-.most-recent-post {
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+
+h1, p {
+margin: 0;
+}
+.hero-container {
+    background-color: aqua;
+    display: flex;
+    justify-content: flex-start;
     width: 100%;
+    height: 200px;
     background-image: url("https://cards.scryfall.io/art_crop/front/1/8/181ed588-5b9e-4160-967c-ed927b7e0048.jpg?1562544319");
-    background-repeat:no-repeat;
+    background-repeat: no-repeat;
     background-size: contain;
+   
+}
+.most-recent-post {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+    
+    border: solid 3px purple;
+    color: white;
+    
+ 
 }
 </style>
