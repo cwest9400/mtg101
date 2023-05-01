f46b09d84e639f87c896ebb20ec261fb65ea638d
cwest9400 <chrstphrwest@gmail.com>
2023-04-30 20:46:20 -0400

    added X when menu active

--- src/components/navbar/NavBarContent.vue
+++ src/components/navbar/NavBarContent.vue
@@ -3,16 +3,38 @@
     <div class="navbar-firstrow-content">
       <h1 class="title">six6wurm</h1>
       <div class="navbar-hamburger" @click="showMenu = !showMenu">
+        <!-- "Hamburger" icon if menu inactive -->
         <svg
+          v-if="!showMenu"
           xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
           width="24"
           height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#000000"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
         >
-          <path
-            fill="currentColor"
-            d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"
-          />
+          <line x1="3" y1="12" x2="21" y2="12"></line>
+          <line x1="3" y1="6" x2="21" y2="6"></line>
+          <line x1="3" y1="18" x2="21" y2="18"></line>
+        </svg>
+        <!-- "X" icon if menu active -->
+        <svg
+          v-else
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#000000"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <line x1="18" y1="6" x2="6" y2="18"></line>
+          <line x1="6" y1="6" x2="18" y2="18"></line>
         </svg>
       </div>
       <div class="navbar-menu" :class="{ 'navbar-menu-visible': showMenu }">
