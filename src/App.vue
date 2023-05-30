b69b95b2f7f239d7055c0fa2f2866af6c74375e8
cwest9400 <chrstphrwest@gmail.com>
2023-05-30 15:58:09 -0400

    modern button, post card padding

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,7 @@ export default {
   text-align: center;
   color: #2c3e50;
   /* margin: 60px 24px 0px 24px; */
-  padding:5% 20% 0 20%;
+  padding:5% 10% 0 10%;
   
 }
 body {
--- src/components/ArticlePost.vue
+++ src/components/ArticlePost.vue
@@ -29,18 +29,48 @@ margin: 0;
 
 }
 
+button {
+  background-color: #5c2b94;
+  color: aliceblue;
+  height: 40px;
+  border: none;
+  border-radius: 20px;
+  padding: 0 20px;
+  font-size: 16px;
+  font-weight: bold;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  box-shadow: 0 2px 4px rgba(0,0,0,0.2);
+}
+
+button:hover {
+  background-color: #4b1f74;
+}
+
+button:focus {
+  outline: none;
+  box-shadow: 0 0 0 3px rgba(92, 43, 148, 0.4);
+}
+
 .hero-container {
   position: relative;
     background-color: rgb(0, 0, 0);
     display: flex;
     justify-content: flex-start;
     width: 100%;
-    height: 200px;
+    height: 100%;
     background-image: url("https://cards.scryfall.io/art_crop/front/5/d/5d275f04-cc60-4e3f-95cc-3d02bc916b82.jpg?1599710280");
     background-repeat: no-repeat;
-    background-size: contain;
-    background-position: right top;
+    
+    background-size: cover;
+    background-position: top center;
     margin-top: 36px;
+    padding: 36px 8px;
+    border-radius: 0 0 8px 8px;
+
+    
    
 }
 
@@ -50,7 +80,7 @@ margin: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  background: linear-gradient(to right, rgba(0,0,0,1) 60%, rgba(0,0,0,0.4220281862745098) 85%);
+  background: linear-gradient(to bottom, rgba(0,0,0,0) 0%, rgba(0,0,0,.6) 90%);
   pointer-events: none; /* allows clicking through the overlay */
 }
 
@@ -65,8 +95,10 @@ margin: 0;
     padding-top: 24px;
     border: solid 3px purple;
     color: white;
+    row-gap: 18px;
     
     
  
 }
+
 </style>
