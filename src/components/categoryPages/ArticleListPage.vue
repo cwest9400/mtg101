4383d5640969f290f7752489142263f21a231ce6
cwest9400 <chrstphrwest@gmail.com>
2023-06-03 21:00:56 -0400

    mapping articles

--- src/components/categoryPages/ArticleListPage.vue
+++ src/components/categoryPages/ArticleListPage.vue
@@ -1,28 +1,38 @@
 <template>
+  <div>
     <h1>List of all the articles page</h1>
-    <p>we want to dynamic render this page with a list of all the articles. When you click on one it loads the detail page.</p>
+    <p>
+      we want to dynamic render this page with a list of all the articles. When
+      you click on one it loads the detail page.
+    </p>
+    <ul>
+      <li v-for="article in articles" :key="article.id">
+        <router-link
+          :to="{ name: 'ArticleDetailPage', params: { id: article.id } }"
+        >
+          {{ article.title }}
+        </router-link>
+      </li>
+    </ul>
+  </div>
 </template>
 <!-- zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz -->
 
 <script>
-import articlesData from '/Users/chriswest/DEV/projects/mtg101/articles.json';
+import articlesData from "/Users/chriswest/DEV/projects/mtg101/articles.json";
 
 export default {
-    data() {
-        return {
-            articles: []
-        };
-    },
-    created() {
-        this.articles = articlesData;
-    }
-
-}
-console.log(articlesData)
+  data() {
+    return {
+      articles: [],
+    };
+  },
+  created() {
+    this.articles = articlesData;
+  },
+};
+console.log(articlesData);
 </script>
 
 <!-- zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz -->
-<style scoped>
-
-
-</style>
+<style scoped></style>
